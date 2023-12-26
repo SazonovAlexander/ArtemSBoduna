f1e3315108ecab6cde1e6d224376d958da4be5ed
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2023-12-27 01:38:03 +0300

    Initial Commit

---
+++ ArtemSBoduna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtemSBoduna
+//
+//  Created by Александр Сазонов on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
